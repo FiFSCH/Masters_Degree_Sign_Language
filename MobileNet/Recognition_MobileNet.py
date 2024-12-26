790fc8035dbe76a34ab6948d90c7bc175d8f71db
Filip Schulz <filipschulz1@gmail.com>
2024-12-27 00:39:48 +0100

    small improvement of models

--- MobileNet/Recognition_MobileNet.py
+++ MobileNet/Recognition_MobileNet.py
@@ -5,17 +5,12 @@ from Auxilary.Live_camera_footage_capture import capture_camera_footage
 
 
 transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(degrees=(-15, 15)),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 # Get class names
-class_names = datasets.ImageFolder(root='../data', transform=transform).classes
+class_names = datasets.ImageFolder(root='./data', transform=transform).classes
 number_of_classes = len(class_names)
 
 # Load pre-trained MobileNet and tuned weights
--- SqueezeNet/Recognition_SqueezeNet.py
+++ SqueezeNet/Recognition_SqueezeNet.py
@@ -3,11 +3,9 @@ import torch.nn as nn
 from torchvision import transforms, models, datasets
 from Auxilary.Live_camera_footage_capture import capture_camera_footage
 
-
 transform = transforms.Compose([
     transforms.Resize(256),
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
